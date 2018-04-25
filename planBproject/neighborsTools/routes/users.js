8e0cb56534f18f68deecdc4acfcf26fc75ea6bf2
AymanGhaith <ayman.ghaith88@gmail.com>
2018-04-25 20:49:08 +0300

    testing the database in heruko , for now i will remove it

--- planBproject/neighborsTools/routes/users.js
+++ planBproject/neighborsTools/routes/users.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-var User = require('../models/Users')
+//var User = require('../models/Users')
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
-  User.find(function(err, users){
-  	res.send(users);
-  });
+  res.json([{id:1, name: "aaaaaaayman"}])
 });
+// router.get('/', function(req, res, next) {
+//   //res.send('respond with a resource');
+//   User.find(function(err, users){
+//   	res.send(users);
+//   });
+// });
 
 module.exports = router;
